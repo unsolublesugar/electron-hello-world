27dc6119233c29ae459f0b25da90b090915cf947
unsoluble_sugar <unsoluble.sugar@gmail.com>
2020-11-23 14:35:33 +0900

    メニューバー非表示

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true
   });
 
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
